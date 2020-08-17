7f5572b933771f648a3c4e04d3b7d85dd9de8c4d
willys_tjiawi <willys@huubap.com>
2020-08-17 15:51:36 +0800

    added questions array

--- lib/main.dart
+++ lib/main.dart
@@ -5,20 +5,29 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  var questionIdx = 0;
+
   void answerQuestion() {
-    print('Button is clicked!!!!!');
+    questionIdx = questionIdx + 1;
+    print(questionIdx);
   }
 
   @override
   Widget build(BuildContext context) {
+    var questions = ['This is question no. 1!', 'This is question no. 2!'];
+
     return MaterialApp(
         home: Scaffold(
             appBar: AppBar(title: Text('My First App')),
             body: Column(children: [
-              Text('Here is the question..... '),
+              Text(questions[questionIdx]),
               RaisedButton(child: Text('Button 1'), onPressed: answerQuestion),
-              RaisedButton(child: Text('Button 1'), onPressed: answerQuestion),
-              RaisedButton(child: Text('Button 1'), onPressed: answerQuestion)
+              RaisedButton(child: Text('Button 2'), onPressed: answerQuestion),
+              RaisedButton(
+                  child: Text('Button 3'),
+                  onPressed: () {
+                    print('Button 3 is clicked!!!!!');
+                  })
             ])));
   }
 }
