aabfc9b67cae3a1f124c2506ddb87ea62c0f1dd3
willys_tjiawi <willys@huubap.com>
2020-08-17 16:46:46 +0800

    added private properties

--- lib/main.dart
+++ lib/main.dart
@@ -4,30 +4,40 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
   var questionIdx = 0;
 
-  void answerQuestion() {
-    questionIdx = questionIdx + 1;
+  void _answerQuestion() {
+    setState(() {
+      questionIdx = questionIdx + 1;
+    });
     print(questionIdx);
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = ['This is question no. 1!', 'This is question no. 2!'];
+    var questions = [
+      'This is question no. 1!',
+      'This is question no. 2!',
+      'This is question no. 3!'
+    ];
 
     return MaterialApp(
         home: Scaffold(
             appBar: AppBar(title: Text('My First App')),
             body: Column(children: [
               Text(questions[questionIdx]),
-              RaisedButton(child: Text('Button 1'), onPressed: answerQuestion),
-              RaisedButton(child: Text('Button 2'), onPressed: answerQuestion),
-              RaisedButton(
-                  child: Text('Button 3'),
-                  onPressed: () {
-                    print('Button 3 is clicked!!!!!');
-                  })
+              RaisedButton(child: Text('Button 1'), onPressed: _answerQuestion),
+              RaisedButton(child: Text('Button 2'), onPressed: _answerQuestion),
+              RaisedButton(child: Text('Button 3'), onPressed: _answerQuestion)
             ])));
   }
 }
