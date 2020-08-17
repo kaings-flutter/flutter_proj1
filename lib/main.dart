bd7739e207cc55008cc1c6026baaa9f96c3febd9
willys_tjiawi <willys@huubap.com>
2020-08-17 15:19:41 +0800

    added event listener to the buttons

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,10 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Button is clicked!!!!!');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -12,9 +16,9 @@ class MyApp extends StatelessWidget {
             appBar: AppBar(title: Text('My First App')),
             body: Column(children: [
               Text('Here is the question..... '),
-              RaisedButton(child: Text('Button 1'), onPressed: null),
-              RaisedButton(child: Text('Button 1'), onPressed: null),
-              RaisedButton(child: Text('Button 1'), onPressed: null)
+              RaisedButton(child: Text('Button 1'), onPressed: answerQuestion),
+              RaisedButton(child: Text('Button 1'), onPressed: answerQuestion),
+              RaisedButton(child: Text('Button 1'), onPressed: answerQuestion)
             ])));
   }
 }
