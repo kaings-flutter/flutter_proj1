f3fe43ac8de0707d044b83a607db6adc6459f83c
willys_tjiawi <willys@huubap.com>
2020-08-17 14:56:16 +0800

    added Column widget to the Scaffold body

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-      appBar: AppBar(
-        title: Text('My First App'),
-      ),
-    ));
+            appBar: AppBar(title: Text('My First App')),
+            body: Column(children: [
+              Text('Here is the question..... '),
+              RaisedButton(child: Text('Button 1'), onPressed: null),
+              RaisedButton(child: Text('Button 1'), onPressed: null),
+              RaisedButton(child: Text('Button 1'), onPressed: null)
+            ])));
   }
 }
